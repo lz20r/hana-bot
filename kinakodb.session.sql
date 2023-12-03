4ec9685f06f00f332426c43b20ea5d958aced6de
nazhiida <zhiyaonaiara@gmail.com>
2023-12-03 23:42:52 +0100

    mysql changes

--- kinakodb.session.sql
+++ kinakodb.session.sql
@@ -1,23 +1,25 @@
 use s109249_kinakodb;
 
-/*
+
 drop table if exists prefijos;
 create table if not exists prefijos(
     id int NOT NULL PRIMARY KEY auto_increment,
     prefix varchar(3) not null,
-    guildId VARCHAR(30) not null unique,
+    guildId VARCHAR(30) not null,
     userid VARCHAR(30) not null,
     fecha timestamp not null
 );
-*/
+SELECT * FROM prefijos;
+
+
 drop table if exists historialPrefix;
 create table if not exists historialPrefix(
     id int NOT NULL PRIMARY KEY auto_increment,
     prefix varchar(3) not null,
-    guildId VARCHAR(30) not null unique,
+    guildId VARCHAR(30) not null,
     userid VARCHAR(30) not null,
     fecha timestamp not null
 );
 
-SELECT * FROM prefijos;
+
 SELECT *FROM historialPrefix;

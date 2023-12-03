371863f649eeb6bde9699050e811adc5cb50a7bb
nazhiida <zhiyaonaiara@gmail.com>
2023-12-03 21:04:00 +0100

    Cambios fallidos

--- Comandos/config/setprefix.js
+++ Comandos/config/setprefix.js
@@ -13,16 +13,15 @@ module.exports = {
         const { prefix } = message
         const { db } = client
         try {
-            if (!args[0]) return message.channel.send(`[${message.author.tag}: 
-                ${message.author.globalName}] had try to change the prefix **${prefix}** of the server **${message.guild.name}** but didn't excify the new one so will mantain the actual prefix **${prefix}**`); 
+            if (!args[0]) return message.channel.send(`[${message.author.tag}: ${message.author.globalName}] had try to change the prefix **${prefix}** of the server **${message.guild.name}** but didn't excify the new one so will mantain the actual prefix **${prefix}**`);
             const newPrefix = args[0];
             const { guildId } = message;
-            const timestamp = new Date().toISOString().slice(0, 19).replace('T', ' '); 
- 
-            const [rows] = await db.execute('SELECT * FROM prefijos WHERE guildId = ?', [guildId]);
+            const timestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+            const [rows] = await db.execute("SELECT * FROM prefijos WHERE guildId = ?", [guildId]);
             if (rows.length > 0) {
-                await db.execute('UPDATE prefijos SET prefix = ?, userid = ?, fecha = ? WHERE guildId = ?', [newPrefix, message.author.id, timestamp, guildId]);
-                db.execute('Insert into historialPrefix (guildId, prefix, userid, fecha) VALUES (?,?,?,?)', [guildId, newPrefix, message.author.id, timestamp])
+                await db.execute("UPDATE prefijos SET prefix = ?, userid = ?, fecha = ? WHERE guildId = ?", [newPrefix, message.author.id, timestamp, guildId]);
+                db.execute("INSERT INTO historialPrefix (guildId, prefix, userid, fecha) VALUES (?,?,?,?)", [guildId, newPrefix, message.author.id, timestamp])
             } else {
                 db.execute("INSERT INTO prefijos (`prefix`, `guildId`, `userid`, `fecha`) VALUES (?,?,?,?)", [newPrefix, guildId, message.author.id, timestamp])
             }
@@ -36,51 +35,9 @@ module.exports = {
                     `**ErrMessage:**\n - \`\`\`${e.message}\`\`\`\n` +
                     `**Stacktrace:**\n - \`\`\`${e.stack}\`\`\`\n`
                 )
-            message.channel.send({ embeds: [Error] }).then((msg) => {
-                setTimeout(() => {
-                    msg.delete
-                })
-            })
+            message.channel.send({ embeds: [Error] })
         } finally {
             console.log(`\n💭  [CMD] ${message.author.globalName} [${message.author.tag}] ha usado el comando ${prefix}${this.name} en el servidor ${message.guild.name}`);
         }
     }
 }
-
-
- /*const server = message.guild.id;
-        try {
-            
-            try {
-                const setPrefix = db.set("prefix." + server, args[0])
-                const getPrefix = await db.get("prefix." + server)
-                if (!args[0]) return message.channel.send(`[${message.author.tag}: 
-                    ${message.author.globalName}] had try to change the prefix **${getPrefix}** of the server **${message.guild.name}** but didn't excify the new one so will mantain the actual prefix **${getPrefix}**`);
-                setPrefix 
-                    .then((x) => {
-                        if (getPrefix) {
-                message.channel.send({ content: `[${message.author.tag}: ${message.author.globalName}] had change the prefix **${getPrefix}** of the server **${message.guild.name}** into  **${args[0]}**` })
-                } else {
-                    message.reply(`An error occurred while changing the prefix by **[${message.author.tag}: ${message.author.globalName}]**  so will mantain the actual ${prefix}`)
-                }
-            })
-        
-            } catch (e) {
-                console.log(e)
-            }
-        } catch (e) {
-            const Error = new EmbedBuilder()
-                .setColor(0xFF0000)
-                .setDescription(
-                    `**Error al ejecutar el comando**\n- \`\`\`${prefix}${this.name}\`\`\`\n\n` +
-                    `**ErrMessage:**\n - \`\`\`${e.message}\`\`\`\n` +
-                    `**Stacktrace:**\n - \`\`\`${e.stack}\`\`\`\n`
-                )
-            message.channel.send({ embeds: [Error] }).then((msg) => {
-                setTimeout(() => {
-                    msg.delete
-                })
-            })
-        } finally {
-            console.log(`\n💭  [CMD] ${message.author.globalName} [${message.author.tag}] ha usado el comando ${prefix}${this.name} en el servidor ${message.guild.name}`);
-        } */
--- kinakodb.session.sql
+++ kinakodb.session.sql
@@ -1,19 +1,23 @@
 use s109249_kinakodb;
+
+/*
 drop table if exists prefijos;
 create table if not exists prefijos(
-    prefix varchar(1) not null,
-    guildId VARCHAR(30) primary key not null unique,
-    userid VARCHAR(30) not null,
-    fecha timestamp not null 
-);
-drop table if exists historialPrefix;
-create table if not exists historialPrefix(
-    prefix varchar(1) not null,
-    guildId VARCHAR(30) primary key not null unique,
+    id int NOT NULL PRIMARY KEY auto_increment,
+    prefix varchar(3) not null,
+    guildId VARCHAR(30) not null unique,
     userid VARCHAR(30) not null,
     fecha timestamp not null
 );
-SELECT *
-FROM prefijos;
-SELECT *
-FROM historialPrefix;
+*/
+drop table if exists historialPrefix;
+create table if not exists historialPrefix(
+    id int NOT NULL PRIMARY KEY auto_increment,
+    prefix varchar(3) not null,
+    guildId VARCHAR(30) not null unique,
+    userid VARCHAR(30) not null,
+    fecha timestamp not null
+);
+
+SELECT * FROM prefijos;
+SELECT *FROM historialPrefix;

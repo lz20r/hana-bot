3a0062db8c61289d43d28df079a176fc5ffbaaa3
Nia <zhiyaonaiara@gmail.com>
2023-12-17 12:50:30 +0100

    Cambios

--- kinakodb.session.sql
+++ kinakodb.session.sql
@@ -4,7 +4,7 @@ use s109249_kinakodb;
 drop table if exists prefijos;
 create table if not exists prefijos(
     id int NOT NULL PRIMARY KEY auto_increment,
-    prefix varchar(3) not null,
+    prefix varchar(30) not null,
     guildId VARCHAR(30) not null,
     userid VARCHAR(30) not null,
     fecha timestamp not null
